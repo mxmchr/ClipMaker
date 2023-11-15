869b4fde0fc77050bad17d05fe5170e52f21e2fe
mxmchr <chretien.maxime72@gmail.com>
2023-11-15 11:47:21 +0100

    new call

--- app/views/ClipsView.php
+++ app/views/ClipsView.php
@@ -1,12 +1,17 @@
 <?php
-
 class ClipsView
 {
     // Méthode pour afficher tous les clips
     public function showAllClips($clips)
     {
         echo "<article class='article__clipsViews'>";
+
             echo "<h1 class='title__clipsView'>Liste des clips</h1>";
+            echo "<div class='search-container'>
+                    <input type='text' class='search-input' placeholder='Rechercher...'>
+                    <button class='search-button'>Rechercher</button>
+                </div>";
+
             echo "<ul class='list__clips'>";
                 foreach ($clips as $clip) {
                     $clipsModel = new ClipsModel();
@@ -19,8 +24,6 @@ class ClipsView
                                 </video>
                                 <h3 class="title__date">' . $formattedDate . '</h3>
                             </li>';
-
-
                 }
             echo "</ul>";
         echo "</article>";
