42b5988e636827500b928324d3641b24307c70e3
mxmchr <chretien.maxime72@gmail.com>
2023-11-24 14:15:36 +0100

    new call

--- app/Router.php
+++ app/Router.php
@@ -8,23 +8,23 @@ $request_uri = ltrim($request_uri, '/');
 // Diviser l'URL en segments
 $segments = explode('/', $request_uri);
 
-// Déterminer le contrôleur et l'action en fonction des segments d'URL
-$controller = (isset($segments[0]) && !empty($segments[0])) ? ucfirst($segments[0]) . 'Controller' : 'HomeController';
+// Déterminer le contrôleur¿l'action, et le paramètre en fonction des segments d'URL
+$controllerName = (isset($segments[0]) && !empty($segments[0])) ? ucfirst($segments[0]) . 'Controller' : 'HomeController';
 $action = (isset($segments[1]) && !empty($segments[1])) ? $segments[1] : 'index';
 $param = (isset($segments[2]) && !empty($segments[2])) ? $segments[2] : null;
 
-// Vérifier si le fichier du contrôleur existe
-$controller_file = "./app/Controllers/{$controller}.php";
-if (file_exists($controller_file)) {
+// Chemin du fichier du contrôleur
+$controllerFile = "./app/Controllers/{$controllerName}.php";
 
+if (file_exists($controllerFile)) {
     // Instancier le contrôleur
-    $fqn=NAMESPACE_CONTROLLER . $controller;
-    $controller_instance = new $fqn();
+    $controllerClassName = NAMESPACE_CONTROLLER . $controllerName;
+    $controllerInstance = new $controllerClassName();
 
     // Vérifier si la méthode/action existe dans le contrôleur
-    if (method_exists($controller_instance, $action)) {
+    if (method_exists($controllerInstance, $action)) {
         // Appeler la méthode/action du contrôleur avec les paramètres
-        $controller_instance->$action($param);
+        $controllerInstance->$action($param);
     }  else {
         // Gérer l'action non trouvée (par exemple, afficher une erreur 404)
         require_once './app/templates/404.php';
@@ -33,5 +33,3 @@ if (file_exists($controller_file)) {
     // Gérer le contrôleur non trouvé (par exemple, afficher une erreur 404)
     require_once './app/templates/404.php';
 }
-
-
